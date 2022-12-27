1d33e6e3e737e02f40856a3cbdd7bfc05e3aad19
gs7joao <gsjoaopedro7@gmail.com>
2022-12-27 11:58:11 -0300

    finalizando projeto curso alura books

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Javascript', id: 1, src:livro },
+    {nome: 'Javascript em missão', id: 2, src:livro },
+    {nome: 'Aventureiros do Javascript', id: 3, src:livro },
+]
